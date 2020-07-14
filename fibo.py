6e8e0628369a38b3400fe340941606dd0a5c8fda
Khushi Kapoor <thekhushikapoor@gmail.com>
2020-07-14 20:03:29 +0530

    assignment 2

---
+++ fibo.py
@@ -0,0 +1,26 @@
+# python3
+
+
+def fibonacci_number_naive(n):
+    assert 0 <= n <= 45
+
+    if n <= 1:
+        return n
+
+    return fibonacci_number_naive(n - 1) + fibonacci_number_naive(n - 2)
+
+
+def fibonacci_number(n):
+    assert 0 <= n <= 45
+
+    l=[]
+    l.append(0)
+    l.append(1)
+    for i in range(2,n+1):
+        l.append(l[i-1]+l[i-2])
+    return l[n]
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(fibonacci_number(input_n))
---
+++ last-digit-of-sum-fibo-again.py
@@ -0,0 +1,52 @@
+# python3
+
+
+def last_digit_of_the_sum_of_fibonacci_numbers_again_naive(from_index, to_index):
+    assert 0 <= from_index <= to_index <= 10 ** 18
+
+    if to_index == 0:
+        return 0
+
+    fibonacci_numbers = [0] * (to_index + 1)
+    fibonacci_numbers[0] = 0
+    fibonacci_numbers[1] = 1
+    for i in range(2, to_index + 1):
+        fibonacci_numbers[i] = fibonacci_numbers[i - 2] + fibonacci_numbers[i - 1]
+
+    return sum(fibonacci_numbers[from_index:to_index + 1]) % 10
+
+
+def last_digit_of_the_sum_of_fibonacci_numbers_again(from_index, to_index):
+    assert 0 <= from_index <= to_index <= 10 ** 18
+    pisano_period = 60
+    n=to_index
+    m=from_index
+    # Taking mod of N with
+    # period length
+    n = n % pisano_period
+    sum1=1
+    previous, current = 0, 1
+    if n==0:
+        sum1=0
+    elif n==1:
+        sum1=1
+    for i in range(n-1):
+        print(previous,current)
+        previous, current = current%10, (previous + current)%10
+        sum1+=current
+    m = m % pisano_period
+    sum2=1
+    previous, current = 0, 1
+    if m==0:
+        sum2=0
+    elif m==1:
+        sum2=1
+    for i in range(m-1):
+        previous, current = current%10, (previous + current)%10
+        sum2+=current
+    return ((sum1-sum2) % 10)
+
+
+if __name__ == '__main__':
+    input_from, input_to = map(int, input().split())
+    print(last_digit_of_the_sum_of_fibonacci_numbers_again(input_from, input_to))
