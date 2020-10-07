89939d701fdd9db2139ed65a40b7a0bba5156e6b
Vansh Sethi <sethivansh6@gmail.com>
2020-10-07 18:39:41 +0530

    workdone

---
+++ java/sumof2digit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class abc{
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the number:");
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      System.out.println(a+b);
+   }
+}
